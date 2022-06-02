35e6e585a75f8127b8d8442e55b5984a4217ff1b
Peter Richard <peter.carlim-ext@viavarejo.com.br>
2022-06-02 10:11:07 -0300

    renomado nome da mainActivity

--- app/src/main/java/br/com/viavarejo/sdkteste/SDKActivity.kt
+++ app/src/main/java/br/com/viavarejo/sdkteste/SDKActivity.kt
@@ -3,9 +3,9 @@ package br.com.viavarejo.sdkteste
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SDKActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sdk)
     }
 }
